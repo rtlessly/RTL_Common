45248bb9166152c441c6a98f79fa4ad6abadcad2
rtlessly <tlessly@hotmail.com>
2018-12-24 12:21:35 -0600

    Minor refactoring

--- RTL_Variant.h
+++ RTL_Variant.h
@@ -8,11 +8,12 @@
 union variant_union_t
 {
     int32_t  Long;
-    int16_t  Int;
-    int8_t   SByte;
     uint32_t UnsignedLong;
+    int16_t  Int;
     uint16_t UnsignedInt;
+    int8_t   SByte;
     uint8_t  Byte;
+	char     Char;
     bool     Bool;
     float    Float;
     void*    Pointer;
@@ -23,10 +24,10 @@ struct variant_t
 {
     variant_t()                     { Data.Long = 0; };
     variant_t(const int32_t value)  { Data.Long = value; };
-    variant_t(const int16_t value)  { Data.Int = value; };
-    variant_t(const int8_t value)   { Data.SByte = value; };
     variant_t(const uint32_t value) { Data.UnsignedLong = value; };
+    variant_t(const int16_t value)  { Data.Int = value; };
     variant_t(const uint16_t value) { Data.UnsignedInt = value; };
+    variant_t(const int8_t value)   { Data.SByte = value; };
     variant_t(const uint8_t value)  { Data.Byte = value; };
     variant_t(const bool value)     { Data.Bool = value; };
     variant_t(const float value)    { Data.Float = value; };
@@ -34,10 +35,10 @@ struct variant_t
     variant_t(const variant_union_t value) { Data = value; };
 
     operator int32_t()  { return Data.Long; };
-    operator int16_t()  { return Data.Int; };
-    operator int8_t()   { return Data.SByte; };
     operator uint32_t() { return Data.UnsignedLong; };
+    operator int16_t()  { return Data.Int; };
     operator uint16_t() { return Data.UnsignedInt; };
+    operator int8_t()   { return Data.SByte; };
     operator uint8_t()  { return Data.Byte; };
     operator bool()     { return Data.Bool; };
     operator float()    { return Data.Float; };
--- Vector3.h
+++ Vector3.h
@@ -20,36 +20,78 @@ template<typename T> struct Vector3
     //*************************************************************************
     // Operations
     //*************************************************************************
+    // Assign a scalar value to all elements of the vector
+    Vector3<T> Set(T rhs) { x = y = z = rhs; return *this; };
+
+    // Zero all elements of the vector
+    Vector3<T> Zero() { x = y = z = 0; return *this; };
+
+    // Determines if all elements of the vector are zero
+    bool IsZero() { return (x == 0 && y == 0 && z == 0); };
+
+    // Compute the magnitude (length) of the vector
     float Magnitude() { return sqrt(x*x + y*y + z*z); };
 
+    // Returns the corresponding unit vector
     Vector3<T> Unit() { float mag = Magnitude(); return Vector3<T>( x/mag, y/mag, x/mag); };
 
     // Cross product c = a x b
     // cx = ay*bz − az*by
     // cy = az*bx − ax*bz
     // cz = ax*by − ay*bx
-
     Vector3<T> Cross(const Vector3<T>& rhs) { return Vector3<T>( y*rhs.z - z*rhs.y, z*rhs.x - x*rhs.z, x*rhs.y - y*rhs.x ); };
 
+    // Compute the dot product of the vector
     T Dot(const Vector3<T>& rhs) { return x*rhs.x + y*rhs.y + z*rhs.x; };
+
+    // Apply a function to each element of the vector and return a new vector containing the result
+    Vector3<T> Apply(T *f(T value)) { return Vector3<T>(f(x), f(y), f(z)); };
+    Vector3<float> Apply(double *f(double value)) { return Vector3<float>(f(x), f(y), f(z)); };
 
     //*************************************************************************
     // Operators
     //*************************************************************************
-    // Vector addition
-    template<typename S> Vector3<T> operator+(const Vector3<S>& rhs) { return Vector3<T>(x+rhs.x, y+rhs.y, z+rhs.z); };
 
-    template<typename S> Vector3<T>& operator+=(const Vector3<S>& rhs) { x += rhs.x; y += rhs.y; z += rhs.z; return *this; };
+    // Assign a scalar value to all elements of the vector
+    template<typename S>
+    Vector3<T> operator=(const S& rhs) { x = y = z = rhs; return *this; };
+
+    // Determine if  to all elements of the vector are equal to a scalar value
+    template<typename S>
+    bool operator==(const S& rhs) { return (x == rhs &&  y == rhs && z == rhs); };
+
+    // Determine if  to any elements of the vector are not equal to a scalar value
+    template<typename S>
+    bool operator!=(const S& rhs) { return !(*this == rhs); };
+
+    // Determine if the vector is equal to another vector
+    template<typename S>
+    bool operator==(const Vector3<S>& rhs) { return (x == rhs.x && y == rhs.y && z == rhs.z); };
+
+    // Determine if the vector is not equal to another vector
+    template<typename S>
+    bool operator!=(const Vector3<S>& rhs) { return !(*this == rhs); };
+
+    // Vector addition
+    template<typename S> 
+    Vector3<T> operator+(const Vector3<S>& rhs) { return Vector3<T>(x+rhs.x, y+rhs.y, z+rhs.z); };
+
+    template<typename S> 
+    Vector3<T>& operator+=(const Vector3<S>& rhs) { x += rhs.x; y += rhs.y; z += rhs.z; return *this; };
 
     // Vector subtraction
-    template<typename S> Vector3<T> operator-(const Vector3<S>& rhs) { return Vector3<T>(x-rhs.x, y-rhs.y, z-rhs.z); };
+    template<typename S> 
+    Vector3<T> operator-(const Vector3<S>& rhs) { return Vector3<T>(x-rhs.x, y-rhs.y, z-rhs.z); };
 
-    template<typename S> Vector3<T>& operator-=(const Vector3<S>& rhs) { x -= rhs.x; y -= rhs.y; z -= rhs.z; return *this; };
+    template<typename S> 
+    Vector3<T>& operator-=(const Vector3<S>& rhs) { x -= rhs.x; y -= rhs.y; z -= rhs.z; return *this; };
 
     // Vector scalar multiplication
-    template<typename S> Vector3<T> operator*(const S s) { return Vector3<T>(x*s, y*s, z*s); };
+    template<typename S> 
+    Vector3<T> operator*(const S s) { return Vector3<T>(x*s, y*s, z*s); };
 
-    template<typename S> Vector3<T>& operator*=(const S s) { x *= s; y *= s; z *= s; return *this; };
+    template<typename S> 
+    Vector3<T>& operator*=(const S s) { x *= s; y *= s; z *= s; return *this; };
 
     // Vector scalar division
     template<typename S> Vector3<T> operator/(const S d) { return Vector3<T>(x/d, y/d, z/d); };
@@ -57,11 +99,13 @@ template<typename T> struct Vector3
     template<typename S> Vector3<T>& operator/=(const S d) { x /= d; y /= d; z /= d; return *this; };
 
     // Vector dot product
-    template<typename S> T operator*(const Vector3<S>& rhs) { return x*rhs.x + y*rhs.y + z*rhs.x; };
+    template<typename S> 
+    T operator*(const Vector3<S>& rhs) { return x*rhs.x + y*rhs.y + z*rhs.x; };
 };
 
 
-template<typename S, typename V>  Vector3<V> operator*(const S s, const Vector3<V>& rhs) { return rhs * s; };
+template<typename S, typename V>  
+Vector3<V> operator*(const S s, const Vector3<V>& rhs) { return rhs * s; };
 
 
 typedef Vector3<float> Vector3F;
